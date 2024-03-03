cc24993dff54bb3e3bb783bdf6abcbef3f4d083c
RolandBuchholz <roland.buchholz@gmx.de>
2024-03-03 19:40:00 +0100

    add wallOpening

--- LiftDataManager/ViewModels/SchachtDetailViewModel.cs
+++ LiftDataManager/ViewModels/SchachtDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.Messaging.Messages;
 using SkiaSharp;
+using SkiaSharp.Views.Windows;
 
 namespace LiftDataManager.ViewModels;
 
@@ -8,6 +9,32 @@ public partial class SchachtDetailViewModel : DataViewModelBase, INavigationAwar
     public SchachtDetailViewModel(IParameterDataService parameterDataService, IDialogService dialogService, INavigationService navigationService) :
      base(parameterDataService, dialogService, navigationService)
     {
+    }
+
+    private readonly string[] shaftDesignParameter = { "var_ZUGANSSTELLEN_A", "var_ZUGANSSTELLEN_B",
+                                                       "var_ZUGANSSTELLEN_C", "var_ZUGANSSTELLEN_D",
+                                                       "var_MauerOeffnungBreiteA", "var_MauerOeffnungAbstandA",
+                                                       "var_MauerOeffnungBreiteB", "var_MauerOeffnungAbstandB",
+                                                       "var_MauerOeffnungBreiteC", "var_MauerOeffnungAbstandC",
+                                                       "var_MauerOeffnungBreiteD", "var_MauerOeffnungAbstandD"};
+
+    public override void Receive(PropertyChangedMessage<string> message)
+    {
+        if (message is null)
+            return;
+        if (!(message.Sender.GetType() == typeof(Parameter)))
+            return;
+        if (message.PropertyName == "var_SB" || message.PropertyName == "var_ST")
+        {
+            SetViewBoxDimensions();
+        };
+
+        if (shaftDesignParameter.Contains(message.PropertyName))
+        {
+            RefreshView();
+        };
+        SetInfoSidebarPanelText(message);
+        _ = SetModelStateAsync();
     }
 
     [ObservableProperty]
@@ -22,28 +49,57 @@ public partial class SchachtDetailViewModel : DataViewModelBase, INavigationAwar
     [ObservableProperty]
     private double shaftDepth;
 
+    private float _stokeWith;
+
+    private SKXamlCanvas? _xamlCanvas;
+
     [RelayCommand]
-    private void OnPaintSurface(SkiaSharp.Views.Windows.SKPaintSurfaceEventArgs e)
+    private void OnPaintSurface(SKPaintSurfaceEventArgs e)
     {
-        //SKImageInfo info = e.Info;
+        SKImageInfo info = e.Info;
         SKSurface surface = e.Surface;
         SKCanvas canvas = surface.Canvas;
         canvas.Clear();
+        canvas.Translate(info.Width / 2, info.Height / 2);
+        _stokeWith = ((float)shaftWidth + (float)shaftDepth) / 600f;
         DrawShaftWall(canvas);
+        DrawEntranceWall(canvas);
+    }
+
+    [RelayCommand]
+    private void OnLoadedSKXamlCanvas(SKXamlCanvas xamlCanvas)
+    {
+        _xamlCanvas = xamlCanvas;
     }
 
     private void DrawShaftWall(SKCanvas canvas)
     {
-        float hatchStokeWith = ((float)shaftWidth + (float)shaftDepth) / 400f;
+        SKPathEffect diagLinesPath = SKPathEffect.Create2DLine(_stokeWith,
+        SkiaSharpHelpers.Multiply(SKMatrix.CreateScale(100f, 100f), SKMatrix.CreateRotationDegrees(45f)));
 
-        SKPathEffect diagLinesPath = SKPathEffect.Create2DLine(hatchStokeWith,
-        SkiaSharpHelpers.Multiply(SKMatrix.CreateScale(hatchStokeWith * 10, hatchStokeWith * 10), SKMatrix.CreateRotationDegrees(45)));
-        SKRect shaftHatch = new(-hatchStokeWith * 10, -hatchStokeWith * 10, (float)ViewBoxWidth, (float)ViewBoxHeight);
-        SKRect shaftWall = new(0f, 0f, (float)ViewBoxWidth, (float)ViewBoxHeight);
+        SKRect shaftWallOutSide = new()
+        {
+            Size = new SKSize((float)(ShaftWidth + 500), (float)(ShaftDepth + 500)),
+            Location = new SKPoint(-(float)((ShaftWidth + 500) / 2), -(float)((ShaftDepth + 500) / 2))
+        };
+
+        SKRect shaftWallHatch = new()
+        {
+            Size = shaftWallOutSide.Size,
+            Location = shaftWallOutSide.Location
+        };
+
+        shaftWallHatch.Inflate(100f,100f);
+
+        SKRect shaftWallInSide = new()
+        {
+            Size = new SKSize((float)ShaftWidth, (float)ShaftDepth),
+            Location = new SKPoint(-(float)(ShaftWidth / 2), -(float)(ShaftDepth / 2))
+        };
 
         using var paintWallSolid = new SKPaint
         {
-            Color = SKColors.DarkGray,
+            Color = SKColors.DimGray,
             IsAntialias = true,
             Style = SKPaintStyle.Fill
         };
@@ -52,33 +108,118 @@ public partial class SchachtDetailViewModel : DataViewModelBase, INavigationAwar
             Color = SKColors.DarkRed,
             PathEffect = diagLinesPath,
             IsAntialias = true,
-            IsStroke = true,
             Style = SKPaintStyle.Stroke
         };
         using var paintHatchOutline = new SKPaint
         {
             Color = SKColors.DarkRed,
             IsAntialias = true,
-            StrokeWidth= hatchStokeWith * 2,
+            StrokeWidth= _stokeWith * 2,
             Style = SKPaintStyle.Stroke
         };
         using var paintShaft = new SKPaint
         {
-            Color = SKColors.LightGray,
+            Color = SKColors.Gray,
             IsAntialias = true,
             Style = SKPaintStyle.Fill
         };
-        canvas.ClipRect(shaftWall);
-        canvas.DrawRect(shaftWall, paintWallSolid);
-        canvas.DrawRect(shaftHatch, paintHatch);
-        canvas.DrawRect(shaftWall, paintHatchOutline);
-        canvas.DrawRect(250f, 250f, (float)ShaftWidth, (float)ShaftDepth, paintShaft);
-        canvas.DrawRect(250f, 250f, (float)ShaftWidth, (float)ShaftDepth, paintHatchOutline);
+
+        canvas.DrawRect(shaftWallOutSide, paintWallSolid);
+        canvas.DrawRect(shaftWallOutSide, paintHatchOutline);
+        canvas.ClipRect(shaftWallOutSide);
+        canvas.DrawRect(shaftWallHatch, paintHatch);
+        canvas.DrawRect(shaftWallInSide, paintShaft);
+        canvas.DrawRect(shaftWallInSide, paintHatchOutline);
+  
+    }
+
+    private void DrawEntranceWall(SKCanvas canvas)
+    {
+        string[] entrances = new[] { "A","B","C","D" };
+
+        foreach (var entrance in entrances)
+        {
+            if (!LiftParameterHelper.GetLiftParameterValue<bool>(ParameterDictionary, $"var_ZUGANSSTELLEN_{entrance}"))
+             continue;
+            float wallOpeningWidth = LiftParameterHelper.GetLiftParameterValue<float>(ParameterDictionary, $"var_MauerOeffnungBreite{entrance}");
+            if (wallOpeningWidth == 0)
+                continue;
+            float wallOpeningLeft = LiftParameterHelper.GetLiftParameterValue<float>(ParameterDictionary, $"var_MauerOeffnungAbstand{entrance}");
+            
+            using var paintShaftEntrace = new SKPaint
+            {
+                Color = SKColors.LightSlateGray,
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill
+            };
+            using var paintOutline = new SKPaint
+            {
+                Color = SKColors.DarkRed,
+                IsAntialias = true,
+                StrokeWidth = _stokeWith * 2,
+                Style = SKPaintStyle.Stroke
+            };
+            using var paintText = new SKPaint
+            {
+                Color = SKColors.DarkRed,
+                IsAntialias = true,
+                TextAlign = SKTextAlign.Center,
+                TextSize = 200,
+            };
+
+            float width = 0f;
+            float height = 0f;
+            float posX = 0f;
+            float posY = 0f;
+
+            switch (entrance)
+            {
+                case "A":
+                    width = wallOpeningWidth;
+                    height = 250f;
+                    posX = -(float)shaftWidth / 2 + wallOpeningLeft;
+                    posY = (float)shaftDepth / 2;
+                    break;
+                case "B":
+                    width = 250f;
+                    height = wallOpeningWidth;
+                    posX = (float)shaftWidth / 2;
+                    posY = (float)shaftDepth / 2 - wallOpeningWidth - wallOpeningLeft;
+                    break;
+                case "C":
+                    width = wallOpeningWidth;
+                    height = -250f;
+                    posX = (float)shaftWidth / 2 - wallOpeningWidth - wallOpeningLeft;
+                    posY = -(float)shaftDepth / 2;
+                    break;
+                case "D":
+                    width = -250;
+                    height = wallOpeningWidth;
+                    posX = -(float)shaftWidth / 2;
+                    posY = -(float)shaftDepth / 2 + wallOpeningLeft;
+                    break;
+                default:
+                    continue;
+            }
+
+            SKRect entranceRect = new()
+            {
+                Size = new SKSize(width, height),
+                Location = new SKPoint(posX, posY)
+            };
+
+            SKRect textRect = new();
+            paintText.MeasureText(entrance, ref textRect);
+
+            canvas.DrawRect(entranceRect, paintShaftEntrace);
+            canvas.DrawRect(entranceRect, paintOutline);
+            canvas.DrawText(entrance, entranceRect.MidX, entranceRect.MidY - textRect.MidY, paintText);
+        }
     }
 
     public void RefreshView()
     {
-        //xamlCanvas.Invalidate();
+        _xamlCanvas?.Invalidate();
     }
 
     private void SetViewBoxDimensions()
@@ -87,8 +228,8 @@ public partial class SchachtDetailViewModel : DataViewModelBase, INavigationAwar
         ShaftDepth = LiftParameterHelper.GetLiftParameterValue<double>(ParameterDictionary, "var_ST");
         if (ShaftWidth > 0 && ShaftDepth > 0)
         {
-            ViewBoxWidth = (ShaftWidth + 500);
-            ViewBoxHeight = (ShaftDepth + 500);
+            ViewBoxWidth = ShaftWidth + 800;
+            ViewBoxHeight = ShaftDepth + 800;
         }
     }
 
