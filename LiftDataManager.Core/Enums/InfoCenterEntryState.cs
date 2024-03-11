d620fcb3c022f22ac67020b6139101396bff420a
RolandBuchholz <roland.buchholz@gmx.de>
2024-03-11 20:21:06 +0100

    add infoCenterTemplateSelector

--- LiftDataManager.Core/Enums/InfoCenterEntryState.cs
+++ LiftDataManager.Core/Enums/InfoCenterEntryState.cs
@@ -2,7 +2,7 @@
 
 namespace LiftDataManager.Core.Enums;
 /// <summary>
-/// InfoCenterEntry State
+/// InfoCenterEntry State with german humanized Displayname
 /// </summary>
 
 public sealed class InfoCenterEntryState : SmartEnum<InfoCenterEntryState>
@@ -10,30 +10,27 @@ public sealed class InfoCenterEntryState : SmartEnum<InfoCenterEntryState>
     //    /// <summary>
     //    /// State not definiert
     //    /// </summary>
-    public static readonly InfoCenterEntryState None = new(nameof(None), 0);
+    public static readonly InfoCenterEntryState None = new("Keine", 0);
     //    /// <summary>
     //    /// State Message
     //    /// </summary>
-    public static readonly InfoCenterEntryState InfoCenterMessage = new(nameof(InfoCenterMessage), 1);
+    public static readonly InfoCenterEntryState InfoCenterMessage = new("Nachricht", 1);
     //    /// <summary>
     //    /// State Warning
     //    /// </summary>
-    public static readonly InfoCenterEntryState InfoCenterWarning = new(nameof(InfoCenterWarning), 2);
+    public static readonly InfoCenterEntryState InfoCenterWarning = new("Warnung", 2);
     //    /// <summary>
     //    /// State Error
     //    /// </summary>
-    public static readonly InfoCenterEntryState InfoCenterError = new(nameof(InfoCenterError), 3);
+    public static readonly InfoCenterEntryState InfoCenterError = new("Fehler", 3);
     //    /// <summary>
-    //    /// State ParameterChanged
+    //    /// State Parameter changed
     //    /// </summary>
-    public static readonly InfoCenterEntryState InfoCenterParameterChanged = new(nameof(InfoCenterParameterChanged), 4);
+    public static readonly InfoCenterEntryState InfoCenterParameterChanged = new("Parameterwert geändert", 4);
     //    /// <summary>
-    //    /// State Save Parameter
+    //    /// State Parameter saved
     //    /// </summary>
-    public static readonly InfoCenterEntryState InfoCenterSaveParameter = new (nameof(InfoCenterSaveParameter), 5);
-    //    /// <summary>
-    //    /// State Error
-    //    /// </summary>
+    public static readonly InfoCenterEntryState InfoCenterSaveParameter = new ("Parameterwert gespeichert", 5);
  
     private InfoCenterEntryState(string name, int value) : base(name, value)
     {
---
+++ LiftDataManager/Controls/TemplateSelectors/InfoCenterEntryStateTemplateSelector.cs
@@ -0,0 +1,57 @@
+﻿namespace LiftDataManager.Controls.TemplateSelectors;
+
+public class InfoCenterEntryStateTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate? None
+    {
+        get; set;
+    }
+    public DataTemplate? InfoCenterMessage
+    {
+        get; set;
+    }
+    public DataTemplate? InfoCenterWarning
+    {
+        get; set;
+    }
+    public DataTemplate? InfoCenterError
+    {
+        get; set;
+    }
+    public DataTemplate? InfoCenterParameterChanged
+    {
+        get; set;
+    }
+    public DataTemplate? InfoCenterSaveParameter
+    {
+        get; set;
+    }
+
+    protected override DataTemplate? SelectTemplateCore(object item)
+    {
+        return SelectTemplateCore(item, null);
+    }
+
+    protected override DataTemplate? SelectTemplateCore(object item, DependencyObject? container)
+    {
+        if (item is not null)
+        {
+            var entryState = (InfoCenterEntryState)item;
+
+            return entryState.Value switch
+            {
+                0 => None,
+                1 => InfoCenterMessage,
+                2 => InfoCenterWarning,
+                3 => InfoCenterError,
+                4 => InfoCenterParameterChanged,
+                5 => InfoCenterSaveParameter,
+                _ => None,
+            };
+        }
+        else
+        {
+            return None;
+        }
+    }
+}
