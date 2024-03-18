e3b24b6d500f7b89a80feff1de945d23998c947b
RolandBuchholz <roland.buchholz@gmx.de>
2024-03-18 17:21:02 +0100

    add BufferCalculationsData

---
+++ LiftDataManager.Core/Models/BufferCalculationData.cs
@@ -0,0 +1,11 @@
+﻿namespace LiftDataManager.Core.Models;
+
+public class BufferCalculationData
+{
+    public required string ProfilDescription { get; set; }
+    public int NumberOfBuffer { get; set; }
+    public required string BufferPillarLength { get; set; }
+    public required string ProfilMaterial { get; set; }
+    public double MomentOfInertiaX { get; set; }
+    public double MomentOfInertiaY { get; set; }
+}
--- LiftDataManager/ViewModels/BausatzDetailViewModel.cs
+++ LiftDataManager/ViewModels/BausatzDetailViewModel.cs
@@ -1,8 +1,26 @@
-﻿namespace LiftDataManager.ViewModels;
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
 
-public partial class BausatzDetailViewModel : ObservableRecipient
+namespace LiftDataManager.ViewModels;
+
+public partial class BausatzDetailViewModel : DataViewModelBase, INavigationAware, IRecipient<PropertyChangedMessage<string>>
 {
-    public BausatzDetailViewModel()
+    private readonly ParameterContext _parametercontext;
+    private readonly ICalculationsModule _calculationsModuleService;
+
+    public BausatzDetailViewModel(IParameterDataService parameterDataService, IDialogService dialogService, INavigationService navigationService, IInfoCenterService infoCenterService, ParameterContext parametercontext, ICalculationsModule calculationsModuleService) :
+         base(parameterDataService, dialogService, navigationService, infoCenterService)
     {
+        _parametercontext = parametercontext;
+        _calculationsModuleService = calculationsModuleService;
+    }
+
+    public void OnNavigatedTo(object parameter)
+    {
+        NavigatedToBaseActions();
+    }
+
+    public void OnNavigatedFrom()
+    {
+        NavigatedFromBaseActions();
     }
 }
