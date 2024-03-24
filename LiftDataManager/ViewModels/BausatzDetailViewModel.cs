6624ee1b2f30dda2319f53d9c959f10f33860a7f
RolandBuchholz <roland.buchholz@gmx.de>
2024-03-24 19:38:43 +0100

    divider style

--- LiftDataManager/ViewModels/BausatzDetailViewModel.cs
+++ LiftDataManager/ViewModels/BausatzDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using LiftDataManager.Core.DataAccessLayer.Models.Fahrkorb;
 
 namespace LiftDataManager.ViewModels;
 
@@ -14,12 +15,53 @@ public partial class BausatzDetailViewModel : DataViewModelBase, INavigationAwar
         _calculationsModuleService = calculationsModuleService;
     }
 
+    public string CarFrameTyp => string.IsNullOrWhiteSpace(ParameterDictionary["var_Bausatz"].Value) ? "Kein Bausatz gewählt" : ParameterDictionary["var_Bausatz"].Value!;
+
+    [ObservableProperty]
+    private bool isCFPFrame;
+
+    [ObservableProperty]
+    private bool isCFPDataBaseOverwritten;
+
+    [ObservableProperty]
+    private bool showCFPFrameInfo;
+
     [RelayCommand]
     private void GoToBausatzViewModel() => _navigationService.NavigateTo("LiftDataManager.ViewModels.BausatzViewModel");
+
+    private void CheckCFPState()
+    {
+        var fangrahmenTyp = ParameterDictionary!["var_Bausatz"].Value;
+        if (string.IsNullOrWhiteSpace(fangrahmenTyp))
+            return;
+        var carFrameType = _parametercontext.Set<CarFrameType>().FirstOrDefault(x => x.Name == fangrahmenTyp);
+        if (carFrameType is null)
+            return;
+        IsCFPFrame = carFrameType.IsCFPControlled;
+        ShowCFPFrameInfo = IsCFPFrame & !LiftParameterHelper.GetLiftParameterValue<bool>(ParameterDictionary, "var_CFPdefiniert");
+        if (IsCFPFrame)
+        {
+            if (string.IsNullOrWhiteSpace(FullPathXml))
+                return;
+
+            var basePath = Path.GetDirectoryName(FullPathXml);
+            if (string.IsNullOrWhiteSpace(basePath))
+                return;
+
+            var calculationsPath = Path.Combine(basePath, "Berechnungen");
+
+            if (Directory.Exists(calculationsPath))
+            {
+                var calculations = Directory.EnumerateFiles(calculationsPath);
+                IsCFPDataBaseOverwritten = calculations.Any(x => x.Contains("DB-Anpassungen"));
+            }
+        }
+    }
 
     public void OnNavigatedTo(object parameter)
     {
         NavigatedToBaseActions();
+        CheckCFPState();
     }
 
     public void OnNavigatedFrom()
