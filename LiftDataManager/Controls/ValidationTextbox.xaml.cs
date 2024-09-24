6e44cacea8619fe3b5c6c1dea3e49f6f59470412
RolandBuchholz <roland.buchholz@gmx.de>
2024-09-24 16:06:34 +0200

    add OnApplyTemplate ValidationTextbox

--- LiftDataManager/Controls/ValidationTextbox.xaml.cs
+++ LiftDataManager/Controls/ValidationTextbox.xaml.cs
@@ -5,4 +5,11 @@ public sealed class ValidationTextbox : TextBox
     {
         this.DefaultStyleKey = typeof(ValidationTextbox);
     }
+
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        //Text = "Hallllo";
+    }
+
 }
