78cb1ad89a0ace10120636bffea30fa03708cceb
RolandBuchholz <roland.buchholz@gmx.de>
2024-09-24 20:42:02 +0200

    add visualStateManager(ValidationTextbox)

--- LiftDataManager/Controls/ValidationTextbox.xaml.cs
+++ LiftDataManager/Controls/ValidationTextbox.xaml.cs
@@ -1,3 +1,4 @@
+
 namespace LiftDataManager.Controls;
 public sealed class ValidationTextbox : TextBox
 {
@@ -9,7 +10,41 @@ public sealed class ValidationTextbox : TextBox
     protected override void OnApplyTemplate()
     {
         base.OnApplyTemplate();
-        //Text = "Hallllo";
+        SetValidationState();
+        TextChanged += ValidationTextbox_TextChanged;
+    }
+    
+    private void ValidationTextbox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        IsVaild = string.IsNullOrWhiteSpace(Text);
+        SetValidationState();
     }
 
+    private void SetValidationState() 
+    {
+        VisualStateManager.GoToState(this, IsVaild ? "IsNotVaild" : "IsVaild", useTransitions: false);
+    }
+
+    public bool IsVaild
+    {
+        get { return (bool)GetValue(IsVaildProperty); }
+        set { SetValue(IsVaildProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsVaildProperty =
+        DependencyProperty.Register(nameof(IsVaild),
+            typeof(bool), 
+            typeof(ValidationTextbox), 
+            new PropertyMetadata(default));
+    public bool EnableStringEmptyCheck
+    {
+        get { return (bool)GetValue(EnableStringEmptyCheckProperty); }
+        set { SetValue(EnableStringEmptyCheckProperty, value); }
+    }
+
+    public static readonly DependencyProperty EnableStringEmptyCheckProperty =
+        DependencyProperty.Register(nameof(EnableStringEmptyCheck), 
+            typeof(bool), 
+            typeof(ValidationTextbox), 
+            new PropertyMetadata(default));
 }
