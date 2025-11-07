98a106f3661524c5d415d92e7820215e522c729d
RolandBuchholz <roland.buchholz@gmx.de>
2025-11-07 13:50:39 +0100

    Add safetyComponent selection

--- LiftDataManager/ViewModels/Dialogs/GenerateSafetyComponentRecordDialogViewModel.cs
+++ LiftDataManager/ViewModels/Dialogs/GenerateSafetyComponentRecordDialogViewModel.cs
@@ -1,6 +1,8 @@
-﻿using LiftDataManager.Core.DataAccessLayer;
-using LiftDataManager.Core.DataAccessLayer.Models;
+﻿using LiftDataManager.Core.DataAccessLayer.Models;
+using LiftDataManager.Core.DataAccessLayer.Models.AntriebSteuerungNotruf;
 using LiftDataManager.Core.DataAccessLayer.Models.Fahrkorb;
+using LiftDataManager.Core.DataAccessLayer.Models.Tueren;
+using System.Collections.ObjectModel;
 
 namespace LiftDataManager.ViewModels.Dialogs;
 
@@ -10,7 +12,7 @@ public partial class GenerateSafetyComponentRecordDialogViewModel : ObservableOb
     private readonly SafetyComponentRecordContext _safetyComponentRecordContext;
     public int LiftCommissionId { get; set; }
     public List<SafetyComponentTyp> SafetyComponentTyps { get; set; }
-    public List<SafetyComponentEntity> SafetyComponents { get; set; }
+    public ObservableCollection<SafetyComponentEntity> SafetyComponents { get; set; }
 
     private readonly ILogger<GenerateSafetyComponentRecordDialogViewModel> _logger;
 
@@ -20,7 +22,6 @@ public partial class GenerateSafetyComponentRecordDialogViewModel : ObservableOb
         _safetyComponentRecordContext = safetyComponentRecordContext;
         SafetyComponentTyps = [.. _parametercontext.Set<SafetyComponentTyp>()];
         SafetyComponents = [];
-        SafetyComponents = [.. _parametercontext.Set<OverspeedGovernor>()];
         SelectedSafetyComponentTyp = SafetyComponentTyps.FirstOrDefault();
         _logger = logger;
     }
@@ -33,10 +34,46 @@ public partial class GenerateSafetyComponentRecordDialogViewModel : ObservableOb
     }
 
     [ObservableProperty]
-    public partial SafetyComponentTyp? SelectedSafetyComponentTyp {  get; set; }
-    partial void OnSelectedSafetyComponentTypChanged(SafetyComponentTyp? value) 
+    public partial SafetyComponentTyp? SelectedSafetyComponentTyp { get; set; }
+    partial void OnSelectedSafetyComponentTypChanged(SafetyComponentTyp? value)
     {
-        SafetyComponentSelectionVisibility = value?.Id != 1;
+        SafetyComponents.Clear();
+        if (value is null)
+        {
+            SafetyComponentSelectionVisibility = false;
+            return;
+        }
+        SafetyComponentSelectionVisibility = value.Id != 1;
+
+        IEnumerable<SafetyComponentEntity> safetyComponents = value.Id switch
+        {
+            1 => [],
+            2 => _parametercontext.Set<SafetyGearModelType>().Where(x => x.SafetyGearTypeId == 1),
+            3 => _parametercontext.Set<SafetyGearModelType>().Where(x => x.SafetyGearTypeId == 2),
+            4 => _parametercontext.Set<LiftBuffer>().Where(x => x.Manufacturer == "P+S"),
+            5 => _parametercontext.Set<LiftBuffer>().Where(x => x.Manufacturer == "OLEO"),
+            6 => _parametercontext.Set<OverspeedGovernor>().AsEnumerable(),
+            7 => _parametercontext.Set<CarDoor>().AsEnumerable(),
+            8 => _parametercontext.Set<ShaftDoor>().AsEnumerable(),
+            9 => [],
+            10 => [],
+            11 => [],
+            12 => _parametercontext.Set<SafetyGearModelType>().Where(x => x.SafetyGearTypeId == 3),
+            13 => _parametercontext.Set<HydraulicValve>().AsEnumerable(),
+            14 => _parametercontext.Set<DriveSafetyBrake>().AsEnumerable(),
+            15 => _parametercontext.Set<OverspeedGovernor>().AsEnumerable(),
+            16=> [],
+            17 => [],
+            18 => [],
+            19 => [],
+            20 => _parametercontext.Set<LiftPositionSystem>().Where(x => x.TypeExaminationCertificateId != 1),
+            21 => [],
+            22 => _parametercontext.Set<LiftDoorTelescopicApron>().AsEnumerable(),
+            _ => [],
+        };
+
+        SafetyComponents.AddRange(safetyComponents);
+
     }
 
     [ObservableProperty]
